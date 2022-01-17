b8ec337db51ce94e50781267003505c5af26edeb
hwan001 <woghks7209@gmail.com>
2022-01-17 16:52:41 +0900

    20220117 hwan

--- 백준_cpp/백준_cpp/소스.cpp
+++ 백준_cpp/백준_cpp/소스.cpp
@@ -7,36 +7,84 @@
 
 using namespace std;
 
-int main() {
+/* 단계별 풀어보기 >> 단어 공부
+* https://www.acmicpc.net/problem/1157
+*/
+void func_1157() {
 	string str;
 	map<char, int> myMap;
 	map<char, int>::iterator iter;
-	pair<int, int> a1;
 
+	int int_max = 0;
 
-	// a~z, A~Z 0으로 초기화
+	char ch_result;
+	int int_cnt = 0;
+
+	// a~z 0으로 초기화
 	for (char c = 'a'; c <= 'z'; c++) {
 		myMap[c] == 0;
 	}
-	for (char c = 'A'; c <= 'Z'; c++) {
-		myMap[c] == 0;
-	}
 
-	// 입력 받고 카운트
+	// 입력받고 소문자 변경 후 카운트
 	cin >> str;
+
 	for (int i = 0; i < str.length(); i++) {
+		str[i] = tolower(str[i]);
 		myMap[str[i]]++;
 	}
 
-	// 전체 출력
+	// max 값 설정
 	for (iter = myMap.begin(); iter != myMap.end(); iter++) {
-		cout << iter->first << ", " << iter->second << endl;
+		if (iter->second > int_max) {
+			int_max = iter->second;
+		}
 	}
 
-	//minmax(myMap);
-	// 대소문자 구분기능, 알파벳 카운팅 기능
-	// 카운팅 된 알파벳 중 가장 많이 사용된 건?
-	// 여러개 인 경우를 알려면?
+	// max 랑 값이 같으면서 cnt 개수가 1인거는 출력, 아니면 ?
+	for (iter = myMap.begin(); iter != myMap.end(); iter++) {
+		if (int_max == iter->second) {
+			int_cnt++;
+			ch_result = iter->first;
+		}
+	}
+
+	if (int_cnt == 1) {
+		printf("%c\n", toupper(ch_result));
+	}
+	else {
+		cout << '?' << endl;
+	}
+}
+
+/* 단계별 풀어보기 >> 문자열 반복
+* https://www.acmicpc.net/problem/2675
+*/
+void func_2675() {
+	int a, b;
+	string str;
+
+	cin >> a;
+
+	for (int i = 0; i < a; i++) {
+		cin >> b >> str;
+		for (int i = 0; i < str.length(); i++) {
+			for (int j = 0; j < b; j++) {
+				cout << str[i];
+			}
+		}
+		cout << endl;
+	}
+}
+
+/* 단계별 풀어보기 >> 단어의 개수
+* https://www.acmicpc.net/problem/1152
+*/
+void func_1152() {
+
+}
+
+int main() {
+	func_1152();
 
 	return 0;
 }
