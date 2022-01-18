bdd771b4679206b77bd8e8b782373bbae9201e1d
hwan001 <woghks7209@gmail.com>
2022-01-18 17:33:17 +0900

    기본수학1 벌집

--- 백준_cpp/백준_cpp/소스.cpp
+++ 백준_cpp/백준_cpp/소스.cpp
@@ -199,11 +199,63 @@ void func_2941() {
 * https://www.acmicpc.net/problem/1316
 */
 void func_1316() {
+	int a;
+	cin >> a;
+	string* arr_str = new string[a];
 
+	for (int i = 0; i < a; i++) {
+		cin >> arr_str[i];
+	}
+
+
+	for (int i = 0; i < a; i++) {
+		arr_str[i] = 1;
+	}
 }
 
+/* 단계별 풀어보기 >> 기본 수학1 >> 손익분기점
+* https://www.acmicpc.net/problem/1712
+*/
+void func_1712() {
+	int a, b, c;
+	//map<char, int> m = { {'A', 1000}, {'B', 70}, {'C', 1}};
+	cin >> a >> b >> c;
+	int unit_profit = c - b;
+
+	if (unit_profit <= 0) {
+		cout << -1;
+	}
+	else {
+		cout << a / unit_profit +1 << endl;
+	}
+}
+
+/* 단계별 풀어보기 >> 기본 수학1 >> 벌집
+* https://www.acmicpc.net/problem/2292
+*/
+void func_2292() {
+	int a;
+	cin >> a;
+	
+	for (int i = 1; true; i++) {
+		if (a == 1) {
+			cout << i;
+			break;
+		}
+
+		a -= (6 * i);
+		
+		if (a <= 0) {
+			cout << i+1;
+			break;
+		}
+	}
+}
+
+
+
 int main() {
-	func_2941();
+	func_1316();
 
 	return 0;
 }
