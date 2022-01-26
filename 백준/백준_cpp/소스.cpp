e22adf7f7a10e724690c6aadc882ef84869f52ae
hwan001 <woghks7209@gmail.com>
2022-01-26 20:31:24 +0900

    2022-01-26 20:31

--- 백준/백준_cpp/소스.cpp
+++ 백준/백준_cpp/소스.cpp
@@ -9,14 +9,7 @@ void func_test() {
         graph[i] = new int[m];
 
 
-     // Visual Studio의 경우
-    /* 변수를 통해서 배열을 동적으로 생성할 때
-    int ** graph = new int * [n];
-    for (int i = 0; i <= n; ++i)
-        graph[i] = new int[m];
-    */
-
-    // 정적으로 선언했기 때문에 값들을 전부 -1로 초기화
+    // 전부 -1로 초기화
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             graph[i][j] = -1;
@@ -33,8 +26,8 @@ void func_test() {
     for (int i = 0; i < m; i++) {
         int u, v;
         scanf("%d %d", &u, &v);
-        //graph[u][v] = graph[v][u] = 1;
-        graph[u][v] = 1;
+        graph[u][v] = graph[v][u] = 1; // 양방향
+        //graph[u][v] = 1; // 단방향
         // 가중치가 있는 경우 단방향의 경우 graph[u][v] = 가중치값;
     }
 
@@ -47,7 +40,7 @@ void func_test() {
     }
 }
 
-void (*func)() = func_test;
+void (*func)() = func_1260;
 
 int main() {
     func();
