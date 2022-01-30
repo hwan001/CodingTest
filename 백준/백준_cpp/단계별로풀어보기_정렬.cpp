d186231af73308d0f61fdd1817c328aab2176529
hwan001 <woghks7209@gmail.com>
2022-01-30 19:19:42 +0900

    2022-01-30 19:19

---
+++ 백준/백준_cpp/단계별로풀어보기_정렬.cpp
@@ -0,0 +1,78 @@
+#include "header.h"
+
+
+/* 단계별 풀어보기 > 정렬 > 수 정렬하기
+* https://www.acmicpc.net/problem/2750
+*/
+void func_2750() {
+	int a;
+	cin >> a;
+
+	for (int i = 0; i < a; i++) {
+		
+	}
+}
+
+/* 단계별 풀어보기 > 정렬 > 수 정렬하기 2
+* https://www.acmicpc.net/problem/2751
+*/
+void func_2751() {
+
+}
+
+/* 단계별 풀어보기 > 정렬 > 수 정렬하기 3
+* https://www.acmicpc.net/problem/10989
+*/
+void func_10989() {
+
+}
+
+/* 단계별 풀어보기 > 정렬 > 통계학
+* https://www.acmicpc.net/problem/2108
+*/
+void func_2108() {
+
+}
+
+/* 단계별 풀어보기 > 정렬 > 소트인사이드
+* https://www.acmicpc.net/problem/1427
+*/
+void func_1427() {
+
+}
+
+/* 단계별 풀어보기 > 정렬 > 좌표 정렬하기
+* https://www.acmicpc.net/problem/11650
+*/
+void func_11650() {
+
+}
+
+/* 단계별 풀어보기 > 정렬 > 좌표 정렬하기 2
+* https://www.acmicpc.net/problem/11651
+*/
+void func_11651() {
+
+}
+
+/* 단계별 풀어보기 > 정렬 > 단어 정렬
+* https://www.acmicpc.net/problem/1181
+*/
+void func_1181() {
+
+}
+
+/* 단계별 풀어보기 > 정렬 > 나이순 정렬
+* https://www.acmicpc.net/problem/10814
+*/
+void func_10814() {
+
+}
+
+
+/* 단계별 풀어보기 > 정렬 > 좌표 압축
+* https://www.acmicpc.net/problem/18870
+*/
+void func_18870() {
+
+}
--- 백준/백준_cpp/단계별로풀어보기_함수.cpp
+++ 백준/백준_cpp/단계별로풀어보기_함수.cpp
@@ -1,7 +1,22 @@
 #include "header.h"
 
-/*
-void func_15596(); // 정수 N개의 합
-void func_4673(); // 셀프 넘버
-void func_1065(); // 한수
+/* 단계별 풀어보기 > 함수 > 정수 N개의 합
+* https://www.acmicpc.net/problem/15596
 */
+void func_15596() {
+
+}
+
+/* 단계별 풀어보기 > 함수 > 셀프 넘버
+* https://www.acmicpc.net/problem/4673
+*/
+void func_4673() {
+
+}
+
+/* 단계별 풀어보기 > 함수 > 한수
+* https://www.acmicpc.net/problem/1065
+*/
+void func_1065() {
+
+}
