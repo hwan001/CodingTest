ff7fe2a25c187fc0f91103bccbe1678e8cc37daa
hwan001 <woghks7209@gmail.com>
2022-04-23 15:01:38 +0900

    2022-04-23 15:-1

--- Baekjoon/백준_cpp/삼성SW역량테스트기출문제.cpp
+++ Baekjoon/백준_cpp/삼성SW역량테스트기출문제.cpp
@@ -46,7 +46,157 @@ void func_14500() {}
 void func_14501() {}
 
 // 연구소 (골드 5)
-void func_14502() {}
+void func_14502_print(int **_map, int n, int m) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << _map[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	cout << "\n";
+}
+
+int func_14502_combination(int n, int r) {
+	if (n == r || r == 0) {
+		return 1;
+	}
+	else {
+		return func_14502_combination(n-1, r-1) + func_14502_combination(n-1, r);
+	}
+}
+
+// bfs
+void func_14502_virusInfect(int n, int m, int **_map, int **_visit, queue<pair<int, int>> q) {
+	pair<int, int> cur;
+	int _x, _y;
+	int direc[4][2] = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
+
+	while (!q.empty()) {
+		cur = q.front();
+		q.pop();
+
+		for (int i = 0; i < 4; i++) {
+			_x = cur.first + direc[i][0];
+			_y = cur.second + direc[i][1];
+
+			if (_x >= n || _x < 0)
+				continue;
+			if (_y >= m || _y < 0)
+				continue;
+
+			// 벽이면 무시
+			if (_map[_x][_y] == 1 || _map[_x][_y] == 2) {
+				continue;
+			}
+
+			// 방문했으면 무시
+			if (_visit[_x][_y] == 1)
+				continue;
+
+			// 방문하지 않았고, 벽이 아니면 q에 등록하고 방문처리
+			_visit[_x][_y] = 1;
+			_map[_x][_y] = 2;
+
+			q.push({_x, _y});
+		}
+	}
+}
+
+void func_14502() {
+	int n, m;
+	int cnt, cnt_1, cnt_2, max_safeZone = 0;
+
+	vector<pair<int, int>> _map_v;
+	queue<pair<int, int>> q;
+
+	cin >> n >> m;
+	
+	// 초기화 및 지도 생성
+	int** _map = new int* [n];
+	int** _map_copy = new int* [n];
+	int** _visit = new int* [n];
+
+	for (int i = 0; i < n; i++) {
+		_map[i] = new int[m];
+		_map_copy[i] = new int[m];
+		_visit[i] = new int[m];
+	}
+
+	// 입력 받기
+	cnt_1 = 0;
+	cnt_2 = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> _map[i][j];
+
+			if (_map[i][j] == 1) {
+				cnt_1++;
+			}
+			else if(_map[i][j] == 2) {
+				cnt_2++;
+			}
+			else {
+				_map_v.push_back({i, j});
+			}
+		}
+	}
+
+	// permutation 전 오름차순 정렬
+	sort(_map_v.begin(), _map_v.end());
+
+	// 0인 좌표 중 랜덤한 3개를 선택한 순열 -> 해당 좌표를 1로 바꾼뒤 bfs해서 max 갱신
+	do {
+		// 초기화, _map 복사 후 아래 좌표는 1로 변경
+		while (!q.empty()) {
+			q.pop();
+		}
+
+		for (int i = 0; i < n; i++) {
+			memcpy(_map_copy[i], _map[i], sizeof(int) * m);
+			memset(_visit[i], 0, sizeof(int) * m);
+		}
+		for (int i = 0; i < 3; i++) {
+			//cout << "{" << _map_v.at(i).first << ", " << _map_v.at(i).second << "} ";
+			_map_copy[_map_v.at(i).first][_map_v.at(i).second] = 1;
+		}
+
+		// 변경된 map에서 바이러스 확산 후 남은 공간 체크 -> 2에서 전부 방문처리후 방문 안된 경우 (벽도 방문처리는 0, 큐에는 x)
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (_map_copy[i][j] == 2) {
+					_visit[i][j] = 1;
+					q.push({ i, j });
+				}
+			}
+		}
+
+		// 위 조건으로 바이러스 확산
+		func_14502_virusInfect(n, m, _map_copy, _visit, q);
+
+		//func_14502_print(_map_copy, n, m);
+		//func_14502_print(_visit, n, m);
+
+		// 남은 공간의 최대값을 확인하고 갱신
+		cnt = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (_map_copy[i][j] == 0) {
+					cnt++;
+				}
+			}
+		}
+		
+		if (cnt > max_safeZone) {
+			max_safeZone = cnt;
+			//func_14502_print(_visit, n, m);
+			//func_14502_print(_map_copy, n, m);
+		}
+
+		reverse(_map_v.begin() + 3, _map_v.end());
+	} while (next_permutation(_map_v.begin(), _map_v.end()));
+
+	cout << max_safeZone << "\n";
+}
 
 // 로봇 청소기 (골드 5)
 void func_14503() {}
--- Baekjoon/백준_cpp/소스.cpp
+++ Baekjoon/백준_cpp/소스.cpp
@@ -1,6 +1,6 @@
 #include "header.h"
 
-void (*func)() = func_1654;
+void (*func)() = func_14502;
 
 int main() {
 	while(1) func();
