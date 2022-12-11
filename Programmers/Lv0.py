df97a0c59435057dd1447b1679f8f6b826fb0394
hwan001 <woghks7209@gmail.com>
2022-12-11 17:23:04 +0900

    Update Lv0.py

--- Programmers/Lv0.py
+++ Programmers/Lv0.py
@@ -20,6 +20,7 @@ def solution_120908(str1, str2):
 # 기사단원의 무기
 # https://school.programmers.co.kr/learn/courses/30/lessons/136798
 def solution_136798(number, limit, power):
+    import math
     list_knight_power = []
 
     for n in range(1, number+1):
@@ -31,3 +32,41 @@ def solution_136798(number, limit, power):
         list_knight_power.append(len(set(list_tmp)))
 
     return sum([power if x > limit else x for x in list_knight_power])
+
+
+# 외계행성의 나이
+# https://school.programmers.co.kr/learn/courses/30/lessons/120834
+def solution_120834(age):
+    dict_age={0:"a", 1:"b", 2:"c", 3:"d", 4:"e", 5:"f", 6:"g", 7:"h", 8:"i", 9:"j"}
+    return "".join([dict_age[int(ch)] for ch in str(age)])
+
+
+# 가장 가까운 같은 글자
+# https://school.programmers.co.kr/learn/courses/30/lessons/142086
+def solution_142086(s):
+    answer = []
+    dict_str={}
+    
+    for i in range(len(s)):
+        ch = s[i]
+        try:
+            answer.append(i - dict_str[ch])
+        except:
+            answer.append(-1)
+        dict_str[ch] = i
+        
+    return answer
+
+
+# 귤 고르기
+# https://school.programmers.co.kr/learn/courses/30/lessons/138476
+def solution_138476(k, tangerine):
+    from collections import Counter
+    answer = 0
+    
+    for x in dict(sorted(Counter(tangerine).items(), key=(lambda x : x[1]), reverse=True)).values():
+        k -= x
+        answer+=1
+        if k <= 0: break
+    
+    return answer
